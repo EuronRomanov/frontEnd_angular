570d7d8b6d6a231905f1f2499041cb59f98f2723
EuronRomanov <juanpablo.gaona@yahoo.com>
2021-05-23 11:13:46 -0500

    semana 3.2 Promise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
+import {  MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import {  MatCheckboxModule} from "@angular/material/checkbox";
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +19,9 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from "./services/dish.service";
@@ -25,6 +33,7 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -36,7 +45,8 @@ import { ContactComponent } from './contact/contact.component';
       HeaderComponent,
       HomeComponent,
       AboutComponent,
-      ContactComponent
+      ContactComponent,
+      LoginComponent
    ],
   imports: [
     BrowserModule,
@@ -45,9 +55,20 @@ import { ContactComponent } from './contact/contact.component';
     MatToolbarModule,
     MatListModule,
     FlexLayoutModule,
+    FormsModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule
+  ],
+  entryComponents:[
+    LoginComponent
   ],
   providers: [
     DishService
