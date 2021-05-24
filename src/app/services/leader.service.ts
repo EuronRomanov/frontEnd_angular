ec5c69206d27880bc33e9d87ed41f93eac3d1967
EuronRomanov <juanpablo.gaona@yahoo.com>
2021-05-24 09:51:42 -0500

    Semana 3 Promise

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -11,13 +11,16 @@ export class LeaderService {
   constructor() { }
 
 
-  public getLeaders():Leader[]{
-    return LEADERS;
+  public getLeaders():Promise<Leader[]>{
+    return Promise.resolve(LEADERS);
   }
 
+  getLeader(id: string): Promise<Leader> {
+    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+  }
 
-  getFeaturedLeader(): Leader {
-    return LEADERS.filter((leader) => leader.featured)[0];
+  getFeaturedLeader(): Promise<Leader> {
+    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
   }
   
 }
